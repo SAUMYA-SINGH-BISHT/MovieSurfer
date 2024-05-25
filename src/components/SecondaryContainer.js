8a6531b7504f0dc4e881ddb7bf557881ccc2fb78
SAUMYA-SINGH-BISHT <saumyabisht17@gmail.com>
2024-05-25 15:18:51 +0530

    Update SecondaryContainer.js

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -6,7 +6,7 @@ const SecondaryContainer = () => {
   return (
     movies.nowPlayingMovies && (
       <div className="bg-black">
-        <div className=" mt-0 md:-mt-20 pl-4 md:pl-12 relative z-20">
+        <div className=" mt-0 md:-mt-20 pl-4 md:pl-12 relative z-20 pb-5">
           <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
           <MovieList title={"Top Rated"} movies={movies.TopRatedMovies} />
           <MovieList title={"Upcoming Movies"} movies={movies.UpcomingMovies} />
